bdf5e53ca00929bfd1d0c8e8a47412fa4f947828
João Natividade <87316339+joaosnet@users.noreply.github.com>
2024-08-14 10:02:18 -0300

    Update views.py

--- dashapp/views.py
+++ dashapp/views.py
@@ -17,15 +17,19 @@ left_column = html.Div(
     id="left-column",
     className="four columns",
     children=[
-        html.H5(children='Métodos Numéricos'), # Título do aplicativo
-        html.Div(children='''Seja bem-vindo ao aplicativo de métodos numéricos! Aqui você poderá visualizar os métodos numéricos implementados e o gráfico.'''),
+        html.H5(children="Métodos Numéricos"),  # Título do aplicativo
+        html.Div(
+            children="""Seja bem-vindo ao aplicativo de métodos numéricos! Aqui você poderá visualizar os métodos numéricos implementados e o gráfico."""
+        ),
         html.Hr(),
-        html.Div(children='''Adicione um intervalo e uma função para visualizar o método da bisseção:'''),
+        html.Div(
+            children="""Adicione um intervalo e uma função para visualizar o método da bisseção:"""
+        ),
         html.Div([
-            html.Label('Intervalo:'),
+            html.Label("Intervalo:"),
             html.Br(),
             dcc.RangeSlider(
-                id='intervalo',
+                id="intervalo",
                 min=-2,
                 max=2,
                 step=0.5,
@@ -33,36 +37,35 @@ left_column = html.Div(
                 marks={i: str(i) for i in range(-10, 11)},
             ),
             html.Br(),
-            html.Label('Função:'),
-            dcc.Input(id='funcao', type='text', value='32*x**2 - 68*x + 21'),
+            html.Label("Função:"),
+            dcc.Input(id="funcao", type="text", value="32*x**2 - 68*x + 21"),
             html.Hr(),
             html.Br(),
-            html.Label('Interações:'),
-            dcc.Input(id='interacoes', type='number', value=3, min=3, max=100, step=1),
+            html.Label("Interações:"),
+            dcc.Input(id="interacoes", type="number", value=3, min=3, max=100, step=1),
             html.Hr(),
             html.Br(),
-            html.Button('Calcular', id='calcular-bissecao', n_clicks=0),
-        ])
-        ] # Descrição do aplicativo
+            html.Button("Calcular", id="calcular-bissecao", n_clicks=0),
+        ]),
+    ],  # Descrição do aplicativo
 )
 
 right_column = html.Div(
-            id="right-column",
-            className="eight columns",
+    id="right-column",
+    className="eight columns",
+    children=[
+        html.Div(
             children=[
-                html.Div(
-                    children=[
-                        html.Hr(),
-                        dcc.Graph(id="graph"),
-                    ],
-                ),
-                html.Div(
-                    id="tabs",
-                    children=[
-                    ],
-                )
+                html.Hr(),
+                dcc.Graph(id="graph"),
             ],
-        )
+        ),
+        html.Div(
+            id="tabs",
+            children=[],
+        ),
+    ],
+)
 
 # Layout do Dashboard Principal
 layout_dashboard = html.Div(
@@ -79,13 +82,18 @@ layout_dashboard = html.Div(
 app.layout = html.Div([
     dcc.Location(id="url", refresh=False),
     # Banner
-    html.Div(id="banner", children=[
-        html.Img(src=app.get_asset_url("fotos_site/logo.png")),
-        # html.H1("Dashapp"),
-        html.Div(id="navbar"),
-    ], className="banner"),
-    html.Div(id="conteudo_pagina")
+    html.Div(
+        id="banner",
+        children=[
+            html.Img(src=app.get_asset_url("fotos_site/logo.png")),
+            # html.H1("Dashapp"),
+            html.Div(id="navbar"),
+        ],
+        className="banner",
+    ),
+    html.Div(id="conteudo_pagina"),
 ])
+
 
 # pathname
 @app.callback(Output("conteudo_pagina", "children"), Input("url", "pathname"))
@@ -94,53 +102,68 @@ def carregar_pagina(pathname):
         if current_user.is_authenticated:
             return layout_dashboard
         else:
-            return dcc.Link("Usuário não autenticado, faça login aqui", "/login", refresh=True)
-    
+            return dcc.Link(
+                "Usuário não autenticado, faça login aqui", "/login", refresh=True
+            )
+
+
 @app.callback(Output("navbar", "children"), Input("url", "pathname"))
 def exibir_navbar(pathname):
     if pathname != "/logout":
         if current_user.is_authenticated:
             if pathname == "/dash/":
                 return html.Div([
-                    dcc.Link("Logout", "/logout", className="button-link", refresh=True),
-                    dcc.Link("Home", "/", className="button-link", refresh=True)
+                    dcc.Link(
+                        "Logout", "/logout", className="button-link", refresh=True
+                    ),
+                    dcc.Link("Home", "/", className="button-link", refresh=True),
                 ])
             else:
                 return html.Div([
                     dcc.Link("Dashboard", "/dash/", className="button-link"),
-                    dcc.Link("Logout", "/logout", className="button-link", refresh=True),
+                    dcc.Link(
+                        "Logout", "/logout", className="button-link", refresh=True
+                    ),
                     # dcc.Link("Nova Tela", "/nova_tela", className="button-link", refresh=True)
                 ])
         else:
             return html.Div([
                 dcc.Link("Login", "/login", className="button-link", refresh=True)
             ])
-    
-@app.callback(Output("homepage_url", "pathname"), Input("botao-criarconta", "n_clicks"), 
-              [State("email", "value"), State("senha", "value")])
+
+
+@app.callback(
+    Output("homepage_url", "pathname"),
+    Input("botao-criarconta", "n_clicks"),
+    [State("email", "value"), State("senha", "value")],
+)
 def criar_conta(n_clicks, email, senha):
     if n_clicks:
         # vou criar a conta
         # verificar se já existe um usuário com essa conta
-        usuario = Usuario.query.filter_by(email=email).first() # finalizar
+        usuario = Usuario.query.filter_by(email=email).first()  # finalizar
         if usuario:
             return "/login"
         else:
             # criar o usuário
             senha_criptografada = bcrypt.generate_password_hash(senha).decode("utf-8")
-            usuario = Usuario(email=email, senha=senha_criptografada) # 123456
+            usuario = Usuario(email=email, senha=senha_criptografada)  # 123456
             database.session.add(usuario)
             database.session.commit()
             login_user(usuario)
             return "/dash/"
-        
-@app.callback(Output("login_url", "pathname"), Input("botao-login", "n_clicks"), 
-              [State("email", "value"), State("senha", "value")])
+
+
+@app.callback(
+    Output("login_url", "pathname"),
+    Input("botao-login", "n_clicks"),
+    [State("email", "value"), State("senha", "value")],
+)
 def criar_conta(n_clicks, email, senha):
     if n_clicks:
         # vou criar a conta
         # verificar se já existe um usuário com essa conta
-        usuario = Usuario.query.filter_by(email=email).first() # finalizar
+        usuario = Usuario.query.filter_by(email=email).first()  # finalizar
         if not usuario:
             return "/dash//"
         else:
@@ -150,110 +173,140 @@ def criar_conta(n_clicks, email, senha):
                 return "/dash/"
             else:
                 return "/erro"
-            
+
+
 # Calculando a bisseção
-@app.callback(Output("tabs", "children"), Input("calcular-bissecao", "n_clicks"), 
-              State("intervalo", "value"), State("funcao", "value"), State("interacoes", "value"))
+@app.callback(
+    Output("tabs", "children"),
+    Input("calcular-bissecao", "n_clicks"),
+    State("intervalo", "value"),
+    State("funcao", "value"),
+    State("interacoes", "value"),
+)
 def calcular_bissecao(n_clicks, intervalo, funcao, interacoes):
     # tratando a função que está em string para uma função que o python entenda
-    Bissecao_obj = Bissecao(intervalo[0], intervalo[1], lambda x: eval(funcao), maxiter=interacoes)  # Aumenta o número máximo de iterações
+    Bissecao_obj = Bissecao(
+        intervalo[0], intervalo[1], lambda x: eval(funcao), maxiter=interacoes
+    )  # Aumenta o número máximo de iterações
     df = Bissecao_obj.get_df()  # Move a definição de df para fora do bloco try/except
     try:
         x = Bissecao_obj.bissecao()
         iteracoes = Bissecao_obj.get_iteracoes()
-        resultado = f'A raiz da função {funcao} no intervalo [{intervalo[0]}, {intervalo[1]}] é {x} com {iteracoes} iterações.'
+        resultado = f"A raiz da função {funcao} no intervalo [{intervalo[0]}, {intervalo[1]}] é {x} com {iteracoes} iterações."
     except RuntimeError as e:
-        resultado = f'O método da bisseção não convergiu após {interacoes} iterações. Erro: {e}'
-    
+        resultado = (
+            f"O método da bisseção não convergiu após {interacoes} iterações. Erro: {e}"
+        )
+
     return [
-        html.H5(children='Método da Bisseção'), # Título do método
-        html.Div(children=resultado), # Resultado do método
+        html.H5(children="Método da Bisseção"),  # Título do método
+        html.Div(children=resultado),  # Resultado do método
         html.Hr(),
-        html.Div(children='Tabela de Iterações:'),
+        html.Div(children="Tabela de Iterações:"),
         html.Table([
-            html.Thead([
-                html.Tr([html.Th(col) for col in df.columns])
-            ]),
+            html.Thead([html.Tr([html.Th(col) for col in df.columns])]),
             html.Tbody([
-                html.Tr([
-                    html.Td(df.iloc[i][col]) for col in df.columns
-                ]) for i in range(len(df))
-            ])
-        ])
+                html.Tr([html.Td(df.iloc[i][col]) for col in df.columns])
+                for i in range(len(df))
+            ]),
+        ]),
     ]
 
+
 # Atualizando o gráfico
-@app.callback(Output("graph", "figure"), Input("calcular-bissecao", "n_clicks"), 
-              State("intervalo", "value"), State("funcao", "value"), State("interacoes", "value"))
+@app.callback(
+    Output("graph", "figure"),
+    Input("calcular-bissecao", "n_clicks"),
+    State("intervalo", "value"),
+    State("funcao", "value"),
+    State("interacoes", "value"),
+)
 def atualizar_grafico(n_clicks, intervalo, funcao, interacoes):
     # tratando a função que está em string para uma função que o python entenda
     x = np.linspace(intervalo[0], intervalo[1], 100)
-    y = eval(funcao.replace('x', 'x'))
+    y = eval(funcao.replace("x", "x"))
 
     # Calcular a raiz usando o método da bisseção
-    bissecao = Bissecao(intervalo[0], intervalo[1], lambda x: eval(funcao.replace('x', 'x')))
+    bissecao = Bissecao(
+        intervalo[0], intervalo[1], lambda x: eval(funcao.replace("x", "x"))
+    )
     raiz = bissecao.bissecao()
 
     return {
-        'data': [
-            {'x': x, 'y': y, 'type': 'scatter', 'name': 'Função'},
-            {'x': [raiz], 'y': [0], 'mode': 'markers', 'name': 'Raiz', 'marker': {'size': 10, 'color': 'red'}}
-        ],
-        'layout': {
-            'title': f'Gráfico da função {funcao}',
-            'xaxis': {
-                'title': 'x'
+        "data": [
+            {"x": x, "y": y, "type": "scatter", "name": "Função"},
+            {
+                "x": [raiz],
+                "y": [0],
+                "mode": "markers",
+                "name": "Raiz",
+                "marker": {"size": 10, "color": "red"},
             },
-            'yaxis': {
-                'title': 'f(x)'
-            }
-        }
+        ],
+        "layout": {
+            "title": f"Gráfico da função {funcao}",
+            "xaxis": {"title": "x"},
+            "yaxis": {"title": "f(x)"},
+        },
     }
-        
+
+
 # ------------------------------------------------------------------------------------------------------
 @server.route("/")
 def homepage():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # @server.route("/1")
 # def homepage1():
 #     return render_template('homepage1.html')
 
-@server.route('/login', methods=['GET', 'POST'])
+
+@server.route("/login", methods=["GET", "POST"])
 def login():
     form_login = FormLogin()
     if form_login.validate_on_submit():
         usuario = Usuario.query.filter_by(email=form_login.email.data).first()
-        if usuario and bcrypt.check_password_hash(usuario.senha.encode("utf-8"), form_login.senha.data):
+        if usuario and bcrypt.check_password_hash(
+            usuario.senha.encode("utf-8"), form_login.senha.data
+        ):
             login_user(usuario, remember=True)
-            return redirect('/dash/')  # Redireciona para o dashboard
-    return render_template('homepage.html', form=form_login)
+            return redirect("/dash/")  # Redireciona para o dashboard
+    return render_template("homepage.html", form=form_login)
 
-@server.route('/criarconta', methods=['GET', 'POST'])
+
+@server.route("/criarconta", methods=["GET", "POST"])
 def criarconta():
     form_criarconta = FormCriarConta()
     if form_criarconta.validate_on_submit():
-        senha = bcrypt.generate_password_hash(form_criarconta.senha.data).decode("utf-8")
-        usuario = Usuario(username=form_criarconta.username.data,
-                          email=form_criarconta.email.data, 
-                          senha=senha)
+        senha = bcrypt.generate_password_hash(form_criarconta.senha.data).decode(
+            "utf-8"
+        )
+        usuario = Usuario(
+            username=form_criarconta.username.data,
+            email=form_criarconta.email.data,
+            senha=senha,
+        )
         database.session.add(usuario)
         database.session.commit()
         login_user(usuario, remember=True)
-        return redirect('/dash/')  # Redireciona para o dashboard
-    return render_template('criarconta.html', form=form_criarconta)
+        return redirect("/dash/")  # Redireciona para o dashboard
+    return render_template("criarconta.html", form=form_criarconta)
 
-@server.route('/logout')
+
+@server.route("/logout")
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("login"))
 
-@server.route('/previsao', methods=['GET', 'POST'])
+
+@server.route("/previsao", methods=["GET", "POST"])
 def previsao():
     from pprint import pprint as pp
+
     dicionario = {}
-    if request.method == 'POST':
+    if request.method == "POST":
         # Obtém os dados do formulário
         data = request.form
 
@@ -305,13 +358,23 @@ def previsao():
         valores_x = pd.DataFrame(dicionario, index=[0])
 
         # Carrega o modelo de aprendizado de máquina pré-treinado
-        modelo = joblib.load(r'C:\Users\joaod\Documents\GitHub\Data-Science-Project\modelo.joblib')
+        modelo = joblib.load(
+            r"C:\Users\joaod\Documents\GitHub\Data-Science-Project\modelo.joblib"
+        )
 
         # Faz a previsão do valor da propriedade com base nos valores de entrada fornecidos pelo usuário
         try:
-            valor_predito = modelo.predict(valores_x)[0]  # Faz a previsão do valor da propriedade
-            return render_template('previsao.html', valor_predito=f'O valor previsto da propriedade é R$ {valor_predito:.2f}')  # Exibe o valor previsto com duas casas decimais
+            valor_predito = modelo.predict(valores_x)[
+                0
+            ]  # Faz a previsão do valor da propriedade
+            return render_template(
+                "previsao.html",
+                valor_predito=f"O valor previsto da propriedade é R$ {valor_predito:.2f}",
+            )  # Exibe o valor previsto com duas casas decimais
         except Exception as e:
-            return render_template('previsao.html', error=f'Ocorreu um erro ao prever o valor da propriedade: {e}')  # Exibe uma mensagem de erro se ocorrer algum problema
+            return render_template(
+                "previsao.html",
+                error=f"Ocorreu um erro ao prever o valor da propriedade: {e}",
+            )  # Exibe uma mensagem de erro se ocorrer algum problema
     else:
-        return render_template('previsao.html')
+        return render_template("previsao.html")
