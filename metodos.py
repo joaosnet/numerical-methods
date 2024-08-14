fc2a70dea2bb1e333ee436c42b9d837c85808fd4
João Natividade <87316339+joaosnet@users.noreply.github.com>
2024-08-14 10:05:11 -0300

    melhorando os metodos

--- metodos.py
+++ metodos.py
@@ -1,7 +1,7 @@
 import pandas as pd
-import numpy as np
 
-class Bissecao:
+
+class metodos_numericos:
     """
     Implementa o método da bisseção para encontrar a raiz de uma função.
 
@@ -14,9 +14,9 @@ class Bissecao:
         xtol (float, optional): A tolerância para a diferença entre as raízes consecutivas. Defaults to 1e-12.
         rtol (float, optional): A tolerância para o valor absoluto da função. Defaults to 1e-12.
         maxiter (int, optional): O número máximo de iterações permitidas. Defaults to 100.
-        full_output (bool, optional): Se True, retorna a raiz e um DataFrame com os valores intermediários. 
+        full_output (bool, optional): Se True, retorna a raiz e um DataFrame com os valores intermediários.
             Se False, retorna apenas a raiz. Defaults to False.
-        disp (bool, optional): Se True, exibe uma mensagem de erro se o método não convergir. 
+        disp (bool, optional): Se True, exibe uma mensagem de erro se o método não convergir.
             Se False, retorna None se o método não convergir. Defaults to True.
 
     Attributes:
@@ -28,8 +28,21 @@ class Bissecao:
         bissecao(): Executa o método da bisseção para encontrar a raiz.
         get_df(): Retorna o DataFrame com os valores intermediários.
         get_iteracoes(): Retorna o número de iterações realizadas.
-    """
-    def __init__(self, a, b, f, args=(), tol=0.00000000000000001, xtol=1e-12, rtol=1e-12, maxiter=100, full_output=False, disp=True):
+    """  # noqa: E501
+
+    def __init__(  # noqa: PLR0913, PLR0917
+        self,
+        a,
+        b,
+        f,
+        args=(),
+        tol=0.00000000000000001,
+        xtol=1e-12,
+        rtol=1e-12,
+        maxiter=100,
+        full_output=False,
+        disp=True,
+    ):
         self.a = a
         self.b = b
         self.tol = tol
@@ -40,10 +53,11 @@ class Bissecao:
         self.maxiter = maxiter
         self.full_output = full_output
         self.disp = disp
-        self.df = pd.DataFrame(columns=["a", "b", "Aproximação da Raiz", "Erro Relativo (%)"])
+        self.df = pd.DataFrame(
+            columns=['a', 'b', 'Aproximação da Raiz', 'Erro Relativo (%)']
+        )
         self.iteracoes = 0
         self.x_anterior = None
-
 
     def bissecao(self):
         """
@@ -54,21 +68,25 @@ class Bissecao:
             pandas.DataFrame: O DataFrame com os valores intermediários, se `full_output` for True.
         Raises:
             RuntimeError: Se o método não convergir após o número máximo de iterações.
-        """
+        """  # noqa: E501
         a = self.a
         b = self.b
         tol = self.tol
         f = self.f
         args = self.args
+        x = None
         while abs(b - a) > tol:
             self.iteracoes += 1
             if self.iteracoes > self.maxiter:
                 if self.disp:
-                    raise RuntimeError("Falha ao convergir após %d iterações, valor é %s" % (self.maxiter, x))
+                    raise RuntimeError(
+                        'Falha ao convergir após %d iterações, valor é %s'
+                        % (self.maxiter, x)
+                    )
                 else:
                     return x
             fa = f(a, *args)
-            fb = f(b, *args)
+            # fb = f(b, *args)
             x = (a + b) / 2
             fx = f(x, *args)
             if self.iteracoes != 1:
@@ -105,24 +123,30 @@ class Bissecao:
             int: O número de iterações realizadas.
         """
         return self.iteracoes
-    
-if __name__ == "__main__":
-    import math
-    f = lambda x: 32*x**2 - 68*x + 21
+
+
+if __name__ == '__main__':
+    # import math
+    # import numpy as np
+
+    def f(x):
+        return 32 * x**2 - 68 * x + 21
+
     # f = lambda x: math.exp(-x) - x
-    bissecao = Bissecao(0, 1, f)
-    raiz = bissecao.bissecao()
-    print("Raiz:", raiz)
-    print("Iterações:", bissecao.get_iteracoes())
-    print(bissecao.get_df())
+    mn = metodos_numericos(0, 1, f)
+    raiz = mn.bissecao()
+    print('Raiz:', raiz)
+    print('Iterações:', mn.get_iteracoes())
+    print(mn.get_df())
 
     from scipy.optimize import bisect
 
-    # Use o método da bisseção para encontrar a raiz da função no intervalo [a, b]
+    # Use o método da bisseção para encontrar a raiz da função no intervalo
+    # [a, b]
     a = 0
     b = 1
     raiz1 = bisect(f, a, b)
 
-    print("Verdadeira Raiz:", raiz1)
- # Comparação
-    print("Diferença:", abs(raiz - raiz1))
+    print('Verdadeira Raiz:', raiz1)
+    # Comparação
+    print('Diferença:', abs(raiz - raiz1))
