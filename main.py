cead70ed696e56a30a06c42be5698fa06588255b
João Natividade <87316339+joaosnet@users.noreply.github.com>
2024-08-24 13:54:19 -0300

    Update main.py to enable production mode in the Dash app and add environment variable support

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import os
 from dashapp import app
 
+
 # Iniciando o aplicativo
-if __name__ == '__main__':
-    # modo de desenvolvimento
-    # app.run(debug=True)
-    # modo de produção
-    app.run_server(debug=False, host='0.0.0.0')
+if __name__ == "__main__":
+    # Definir o modo a partir da variável de ambiente
+    mode = os.getenv("DEBUG")
+    if mode == 0:
+        app.run_server(debug=True)
+    elif mode == 1:
+        app.run_server(debug=False, host="0.0.0.0")
+    else:
+        app.run_server(debug=True)
